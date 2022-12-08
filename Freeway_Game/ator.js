f4a62894e33a943a224041d6bfb0de674cc30b0a
unknown <patricksteolamartins3@gmail.com>
2022-12-08 14:01:10 -0300

    Jogo Freeway em JavaScript

---
+++ Freeway_Game/ator.js
@@ -0,0 +1,61 @@
+// Ator
+
+let yAtor = 366;
+let xAtor = 85;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image (imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if(keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    if(podeSeMover())    
+    yAtor += 3;
+  }
+}
+
+function verificaColisao(){
+  // collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i ++ ){
+    colisao = collideRectCircle (xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+    if (colisao){
+      posicaoInicial();
+      colidiu.play();
+      if (pontosMaiorQueZero()){
+        meusPontos -= 1;
+      }
+    }
+  }
+}
+
+function posicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(75,0,130);
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+  if (yAtor < 6){
+    meusPontos += 1;
+    ponto.play();
+    posicaoInicial();
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover (){
+  return yAtor < 366
+}
