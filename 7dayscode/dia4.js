46ad75fb9985151e189fc64f6d9cfcb41d31786f
unknown <patricksteolamartins3@gmail.com>
2023-01-11 14:15:53 -0300

    Corrigindo problema do dia 4

--- 7dayscode/dia4.js
+++ 7dayscode/dia4.js
@@ -1,6 +1,7 @@
 const prompt = require("prompt-sync")();
 
 let adivinhe;
+let acertou = false;
 
 function numeroAleatorio (){
    return Math.floor(Math.random() * (10 - 0 + 1) + 0);
@@ -10,7 +11,10 @@ for(let i=0;i<3;i++){
     adivinhe = prompt("Adivinhe um número de 0 a 10. R:");
     if(adivinhe == numeroAleatorio()){
         console.log("Parabéns você acertou!!");
+        acertou = true;
         break;
     }
 }
+if(!acertou){
 console.log("Você errou, que pena!! O número era: "+numeroAleatorio());
+}
